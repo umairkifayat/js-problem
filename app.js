c0d556aed8fdb90535819f064c44461453a18e95
umair kifayat <umairkifayat3@gmail.com>
2023-10-20 21:46:16 -0700

    'animation'

--- app.js
+++ app.js
@@ -1,63 +1,91 @@
-const div = document.querySelector('.div');
-const btn = document.querySelector('.btn');
+// const div = document.querySelector('.div');
+// const btn = document.querySelector('.btn');
 
-const obj = [
-    {
-        name: "Alice",
-        age: 30
-    },
-    {
-        name: "Bob",
-        age: 25
-    },
-    {
-        name: "Charlie",
-        age: 35
-    },
-    {
-        name: "David",
-        age: 28
-    },
-    {
-        name: "Eva",
-        age: 22
-    },
-    {
-        name: "Frank",
-        age: 40
-    },
-    {
-        name: "Grace",
-        age: 27
-    },
-    {
-        name: "Helen",
-        age: 33
-    },
-    {
-        name: "Ian",
-        age: 29
-    },
-    {
-        name: "Jasmine",
-        age: 24
-    },
-];
+// const obj = [
+//     {
+//         name: "Alice",
+//         age: 30
+//     },
+//     {
+//         name: "Bob",
+//         age: 25
+//     },
+//     {
+//         name: "Charlie",
+//         age: 35
+//     },
+//     {
+//         name: "David",
+//         age: 28
+//     },
+//     {
+//         name: "Eva",
+//         age: 22
+//     },
+//     {
+//         name: "Frank",
+//         age: 40
+//     },
+//     {
+//         name: "Grace",
+//         age: 27
+//     },
+//     {
+//         name: "Helen",
+//         age: 33
+//     },
+//     {
+//         name: "Ian",
+//         age: 29
+//     },
+//     {
+//         name: "Jasmine",
+//         age: 24
+//     },
+// ];
 
 
 
-btn.addEventListener('click', () => {
-    obj.map((item) => {
+// btn.addEventListener('click', () => {
+//     obj.map((item) => {
         
-        div.innerHTML += `<h1>Name:${item.name}</h1>
-    <p>Age:${item.age}</p>`
+//         div.innerHTML += `<h1>Name:${item.name}</h1>
+//     <p>Age:${item.age}</p>`
 
-    const cart = JSON.stringify(obj);
-    console.log(obj);
-    localStorage.setItem("obj", cart)
-    });
-})
+//     const cart = JSON.stringify(obj);
+//     console.log(obj);
+//     localStorage.setItem("obj", cart)
+//     });
+// })
 
-function gocart(obj) {
-    window.location = 'cart.html'
-}
+// function gocart(obj) {
+//     window.location = 'cart.html'
+// }
+
+
+
+// const hello =document.querySelector('.hello');
+// const div = document.querySelector('.div');
+
+// console.log(hello);
+//  let interval = 0; 
+// interval = setInterval(() => {
+//     interval++
+//     div.innerHTML = interval 
+//     // console.log(div);
+// },1000)
+
+// interval = setTimeout(() => {
+//     // interval++
+//     div.innerHTML = interval
+// }, 1000);
+
+
+// function updateDiv() {
+//     interval++;
+//     div.innerHTML = interval;
+//     setTimeout(updateDiv, 1000);
+// }
+
+// updateDiv();
+
